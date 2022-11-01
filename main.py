46c31a48ef1a5d7c8d823221a84851f2518624e9
Bekzod1999 <developmentfirm1999@gmail.com>
2022-11-01 10:03:00 +0500

    sendPhoto

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
+from email import message
 import telegram
 TOKEN='5749805943:AAHm17X_M1Sof1MZBCGT-cZE3dGYGxeSPfw'
 bot = telegram.Bot(token=TOKEN)
 
-#sendMessage
-
 new_id = -1
 while True:
     update = bot.getUpdates()[-1]
-    chat_id = update['message']['chat']['id']
-    message_id = update['message']['message_id']
-    text = update['message']['text']
+    chat_id = update.message.chat.id
+    message_id = update.message.message_id
+    text = update.message.text.lower()
 
-    if new_id != message_id:
+    # sendPhoto
+    imgNet = 'https://c.ndtvimg.com/2020-08/h5mk7js_cat-generic_625x300_28_August_20.jpg'
+    img = open('eagle.jpg', 'rb')
+    if new_id != message_id and text == 'send photo':
+        bot.sendPhoto(chat_id, photo = img)
+        new_id = message_id
+
+    #sendMessage
+    elif new_id != message_id:
         bot.sendMessage(chat_id, text)
         new_id = message_id
